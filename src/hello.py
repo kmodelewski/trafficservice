90ce2f60a6f07673714c5383f1c33a5e022072e7
Krzysztof Modelewski <tojakrzysiek@autograf.pl>
2025-02-08 23:16:46 +0100

    new_files

--- src/hello.py
+++ src/hello.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from trafficservice!")
+from enum import Enum
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/{name}")
+def read_root(name):
+    return {"Hello": name}
+
+
+class TrafficIncident(str, Enum):
+    accident = 'accident'
+    location = 'location'
+    
+@app.get("/traffic/all")
+def show_all_incidents(page_size: int = 10):
+    return {"page_size": page_size}
+    
+@app.get("/traffic/{incident}")
+def show_incident(incident: TrafficIncident):
+    return {"incident": incident}
